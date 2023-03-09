d6f6489119492aa5fa1690111d70f5f3ae9eced7
shashwath sk <shashukarkera01@gmail.com>
2023-03-10 00:57:20 +0530

    feat: provide content type context to routes using content type

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,63 @@
-import logo from './logo.svg';
-import React from 'react';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import * as React from 'react';
+import axios from 'axios';
+import {
+    HomePage, PageNotFound, ErrorPage
+} from './pages';
+import { HOME_ROUTE, ERROR_ROUTE } from './constants/routes';
+// import makeRequest from './utils/makeRequest';
+// import { GET_ALL_CONTENTS } from './constants/apiEndPoint';
+import ContentTypeContext from './context/contentTypeContext';
 
 function App() {
+
+    const [ContentType, setContentType] = React.useState();
+    React.useEffect(() => {
+        // makeRequest(GET_ALL_CONTENTS).then((response) => ()=>{
+        //     console.log('sd',response.data);
+        //     setContentType(response.data);
+        // });
+        // console.log('ContentType', ContentType);
+
+        const getContents =async()=>{
+            const response = await axios('http://localhost:5050/contents');
+            setContentType(response.data);
+        };
+        getContents();
+    },[]);
+    const value = { ContentType, setContentType };
+
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-          Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-          Learn React
-                </a>
-            </header>
+
+            <ContentTypeContext.Provider value={value}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path={HOME_ROUTE} element={<HomePage />} />
+                    
+                        <Route path={ERROR_ROUTE} element={<ErrorPage />} />
+                        <Route path="*" element={<PageNotFound />} />
+                    </Routes>
+                </BrowserRouter>
+            </ContentTypeContext.Provider>
+
         </div>
     );
 }
 
 export default App;
+
+
+
+{/* <ThemeContext.Provider value={value}>
+    <Header />
+    <Routes>
+        <Route path={HOME_ROUTE} element={<HomePage />} />
+        <Route path={EVENT_ROUTE} element={<EventPage />} />
+        <Route path={ERROR_ROUTE} element={<ErroPage />} />
+
+        <Route path="*" element={<PageNotFound />} />
+    </Routes>
+    <Footer />
+</ThemeContext.Provider>; */}
