019a2f07858c296b1799d245e3323e0a5dc1350a
shashwath sk <shashukarkera01@gmail.com>
2023-03-10 01:04:27 +0530

    feat: add feature to display input field with search button based on placeholder value

---
+++ src/components/SearchField/index.jsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import search1 from '../../assets/search/search1.png';
+import PropTypes from 'prop-types';
+import './SearchField.css';
+
+export default function SearchField( { placeholder } ) {
+    return(
+        <div className='filter-container'>
+            <input type="text" placeholder={placeholder} />
+            <img src={search1} alt="search" />
+        </div>
+    );
+
+}
+
+
+SearchField.propTypes = {
+    placeholder: PropTypes.string.isRequired,
+};
