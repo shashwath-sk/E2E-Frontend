0a6f9e21a88ac216503a7c7837f6108373224a09
shashwath sk <shashukarkera01@gmail.com>
2023-03-10 01:08:32 +0530

    chore: define all componets need to be exported

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as ViewBar } from './Viewbar';
+export { default as SearchField } from './SearchField';
+export { default as Header } from './Header';
+export { default as AddTextField } from './AddTextField';
+
