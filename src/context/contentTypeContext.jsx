38920bfb045015422ea3848779a5f29295790301
shashwath sk <shashukarkera01@gmail.com>
2023-03-10 01:01:41 +0530

    chore: content type context setup

---
+++ src/context/contentTypeContext.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const ContentTypeContext = React.createContext({
+    ContentType:[],
+    setContentType: () => {}
+});
+
+export default ContentTypeContext;
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as HomePage } from './HomePage';
+export { default as ErrorPage } from './ErrorPage';
+export { default as PageNotFound } from './PageNotFound';
+// export { default as EventPage } from './Eventpage';
+
