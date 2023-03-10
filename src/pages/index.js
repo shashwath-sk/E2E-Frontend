8eb3c6bed5cbe867d42e277b9f1ac10755148070
shashwath sk <shashukarkera01@gmail.com>
2023-03-10 20:27:39 +0530

    chore: add default exports for pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,4 +2,7 @@ export { default as HomePage } from './HomePage';
 export { default as ErrorPage } from './ErrorPage';
 export { default as PageNotFound } from './PageNotFound';
 export { default as DashBoard } from './DashBoard';
+export { default as Register } from './Register';
+export { default as Login } from './Login';
+
 
