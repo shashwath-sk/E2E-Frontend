9f89f9a0f6af423042aebeccd2a9d39fb9bf30e2
shashwath sk <shashukarkera01@gmail.com>
2023-03-10 20:25:38 +0530

    feat: add feature to display each content type entries and enable them to update

--- src/pages/DashBoard/index.jsx
+++ src/pages/DashBoard/index.jsx
@@ -2,129 +2,100 @@ import React from 'react';
 import edit1 from '../../assets/edit/edit1.png';
 import delete1 from '../../assets/delete/delete1.png';
 import makeRequest from '../../utils/makeRequest';
-import {GET_CONTENT_BY_ID} from '../../constants/apiEndPoint';
+import {GET_CONTENT_BY_ID,GET_CONTENT_ENTRIES, DELETE_CONTENT_ENTRIES} from '../../constants/apiEndPoint';
 import './DashBoard.css';
-
-import { AddNewEntryModel, ViewBar } from '../../components';
+import { AddNewEntryModel, ViewBar,Header } from '../../components';
 import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function DashBoard() {
     const [fields, setFields] = React.useState({});
     const [showModal, setShowModal] = React.useState(false);
+    const navigate = useNavigate();
+    const[content,setContent] = React.useState();
+    // const {ContentType,setContentType} = React.useContext(ContentTypeContext);
+    const [contentEntries,setContentEntries] = React.useState();
 
     const {id} = useParams();
     React.useEffect(()=>{
-        makeRequest(GET_CONTENT_BY_ID(id))
+        makeRequest(GET_CONTENT_BY_ID(id),navigate)
             .then((response)=>{
-                console.log(response.Fields);
-                setFields(response.Fields);
+                setFields(Object.keys(response.Fields));
+                setContent(response);
             });
+
+        makeRequest(GET_CONTENT_ENTRIES(id),navigate)
+            .then((response=>{
+                setContentEntries(response);
+                console.log(response);
+            }));
     },[]);
 
+    const handleDelete = async(entryId) => {
+        // url: `content/${ContentID}/entries/:${entryId}`,
+        console.log(DELETE_CONTENT_ENTRIES(1,3).url);
+        const response = await makeRequest(DELETE_CONTENT_ENTRIES(content.id,entryId),navigate);
+        console.log(response);
+    };
+
     
-    return (
+    return content && contentEntries? (
         <div className="details">
             <ViewBar/>
-            <div className="content-entry-container">
-                <div className="content-entrie-title">
-                    <span className="entrie-count">13 Entries Found</span>
-                    <div onClick={() => setShowModal(true)} className="new-entry-div">
-                        <span className="new-entry-text">Add a new entry</span>
+            <div>
+                <Header textValue = {content.Name}/>
+                <div className="content-entry-container">
+                    <div className="content-entrie-title">
+                        <span className="entrie-count">{`${contentEntries.length} Entries Found`}</span>
+                        <div onClick={() => setShowModal(true)} className="new-entry-div">
+                            <span className="new-entry-text">Add a new entry</span>
+                        </div>
+                        <AddNewEntryModel onClose={() => setShowModal(false)} show={showModal} fields={fields} contentId={content.id} contentName={content.Name}/>
                     </div>
-                    <AddNewEntryModel onClose={() => setShowModal(false)} show={showModal} value={fields}/>
-                </div>
-                <div className="table-content-title">
-                    <div className="content-entries-fields">
+                    <div className="table-content-title">
+                        <div className="content-entries-fields">
+                            <span>Id</span>
+                            {
+                                (fields.slice(0,3)).map((key,id) => {
+                                    return (
+                                        <div key={id}>
+                                            <span>{key}</span>
+                                        </div>
+                                    );
+                                })
+                            }
+                        </div>
+                        <span>Actions</span>
+                    </div>
+                    <div className="content-entries">
                         {
-                            Object.keys(fields).map((key,id) => {
+                            Object.keys(contentEntries).map((key,id) => {
                                 return (
-                                    <div key={id}>
-                                        <span>{key}</span>
+                                    <div key={id} className="content-fields-container">
+                                        <div
+                                            className="content-entries-fields"
+                                            style={{ marginLeft: '10px' }}>     
+                                            <span>{contentEntries[key].id}</span>
+                                            <span>{contentEntries[key].Fields[fields[0]]}</span>
+                                            <span>{contentEntries[key].Fields[fields[1]]}</span>
+                                            <span>{contentEntries[key].Fields[fields[2]]}</span>
+                                        </div>
+                                        <div className="content-entries-actions">
+                                            <img src={edit1} alt="edit" />
+                                            <img src={delete1} alt="delete" onClick={()=>handleDelete(contentEntries[key].id)} />
+                                        </div>
                                     </div>
                                 );
-                            })
-                        }
-                    </div>
-                    <span>Actions</span>
-                </div>
-                <div className="content-entries">
-                    <div className="content-fields-container">
-                        <div
-                            className="content-entries-fields"
-                            style={{ marginLeft: '10px' }}
-                        >
-                            <span>1</span>
-                            <span>John Doe</span>
-                            <span>www.johndoe.com</span>
-                            <span>Text</span>
-                        </div>
-                        <div>
-                            <img className="icon" src={edit1} alt="" />
-                            <img className="icon" src={delete1} alt="" />
-                        </div>
-                    </div>
-                    <div className="content-fields-container">
-                        <div
-                            className="content-entries-fields"
-                            style={{ marginLeft: '10px' }}
-                        >
-                            <span>1</span>
-                            <span>John Doe</span>
-                            <span>www.johndoe.com</span>
-                            <span>Text</span>
-                        </div>
-                        <div>
-                            <img className="icon" src={edit1} alt="" />
-                            <img className="icon" src={delete1} alt="" />
-                        </div>
-                    </div>
-                    <div className="content-fields-container">
-                        <div
-                            className="content-entries-fields"
-                            style={{ marginLeft: '10px' }}
-                        >
-                            <span>1</span>
-                            <span>John Doe</span>
-                            <span>www.johndoe.com</span>
-                            <span>Text</span>
-                        </div>
-                        <div>
-                            <img className="icon" src={edit1} alt="" />
-                            <img className="icon" src={delete1} alt="" />
-                        </div>
-                    </div>
-                    <div className="content-fields-container">
-                        <div
-                            className="content-entries-fields"
-                            style={{ marginLeft: '10px' }}
-                        >
-                            <span>1</span>
-                            <span>John Doe</span>
-                            <span>www.johndoe.com</span>
-                            <span>Text</span>
-                        </div>
-                        <div>
-                            <img className="icon" src={edit1} alt="" />
-                            <img className="icon" src={delete1} alt="" />
-                        </div>
-                    </div>
-                    <div className="content-fields-container">
-                        <div
-                            className="content-entries-fields"
-                            style={{ marginLeft: '10px' }}
-                        >
-                            <span>1</span>
-                            <span>John Doe</span>
-                            <span>www.johndoe.com</span>
-                            <span>Text</span>
-                        </div>
-                        <div>
-                            <img className="icon" src={edit1} alt="" />
-                            <img className="icon" src={delete1} alt="" />
-                        </div>
+                            })   
+                        }                         
                     </div>
                 </div>
             </div>
         </div>
-    );
+    ):
+        (
+            <div>
+                <h1>loading</h1>
+            </div>
+        );
 }
